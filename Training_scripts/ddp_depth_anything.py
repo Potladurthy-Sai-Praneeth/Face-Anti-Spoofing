c539e412d9fba4295cf124fb928a550b4f36fa9d
Potladurthy <spotladurthy@ucsd.edu>
2025-06-18 11:48:57 -0700

    Updated by removing large model

--- Training_scripts/ddp_depth_anything.py
+++ Training_scripts/ddp_depth_anything.py
@@ -45,41 +45,90 @@ def cleanup():
     dist.destroy_process_group()
 
 # %%
+# class CustomDataset(Dataset): 
+#     def __init__(self, path, img_size=(128, 128)):
+#         super(CustomDataset, self).__init__()
+#         self.images = []
+#         self.labels = []
+#         self.img_size = img_size
+#         self.transform = transforms.Compose([ transforms.Resize(img_size), transforms.ToTensor()])
+#         self.path = path
+#         self.num_channels = 1
+        
+#         for folder in os.listdir(self.path):
+#             label = 1 if 'client' in folder else 0
+#             for image in os.listdir(os.path.join(self.path, folder)):
+#                 if image.endswith('.jpg') or image.endswith('.png'):
+#                     img_path = os.path.join(self.path, folder, image)
+#                     self.images.append(img_path)
+#                     self.labels.append(label)
+        
+#     def __len__(self):
+#         return len(self.images)
+
+#     def __getitem__(self, idx):
+#         img = Image.open(self.images[idx]).convert("RGB")
+
+#         if self.transform:
+#             img= self.transform(img)
+            
+#         return img, self.labels[idx]
+
+
 class CustomDataset(Dataset): 
     def __init__(self, path, img_size=(128, 128)):
         super(CustomDataset, self).__init__()
-        self.images = []
+        self.client_names = []
+        self.imposter_names =[]
         self.labels = []
+        self.images = []
+        self.ground_truth = []
         self.img_size = img_size
         self.transform = transforms.Compose([ transforms.Resize(img_size), transforms.ToTensor()])
         self.path = path
         self.num_channels = 1
-        
+
         for folder in os.listdir(self.path):
-            label = 1 if 'client' in folder else 0
-            for image in os.listdir(os.path.join(self.path, folder)):
-                if image.endswith('.jpg') or image.endswith('.png'):
-                    img_path = os.path.join(self.path, folder, image)
-                    self.images.append(img_path)
-                    self.labels.append(label)
+            if folder == 'Processed_client':
+                self.client_names.extend(os.listdir(os.path.join(self.path, folder)))
+            if folder == 'Processed_imposter':
+                self.imposter_names.extend(os.listdir(os.path.join(self.path, folder)))
+        
+        for image in self.client_names:
+            img_path = os.path.join(self.path, 'Processed_client', image)
+            self.images.append(img_path)
+            self.labels.append(1)
+            gt_path = os.path.join(self.path, 'gt_client', image)
+            self.ground_truth.append(gt_path)
+        
+        for image in self.imposter_names:
+            img_path = os.path.join(self.path, 'Processed_imposter', image)
+            self.images.append(img_path)
+            self.labels.append(0)
+            gt_path = os.path.join(self.path, 'gt_imposter', image)
+            self.ground_truth.append(gt_path)
+       
         
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
         img = Image.open(self.images[idx]).convert("RGB")
+        gt = Image.open(self.ground_truth[idx]).convert("L")
 
         if self.transform:
             img= self.transform(img)
+            gt = self.transform(gt)
             
-        return img, self.labels[idx]
+        return img, self.labels[idx], gt
 
 def collate_fn(batch):
-  images, labels = zip(*batch)
+  images, labels, gt = zip(*batch)
   images = torch.stack(images, 0)
   labels = torch.tensor(labels)
+  gt = torch.stack(gt, 0)
   
-  return images, labels
+  return images, labels, gt
 
 # %%
 def main_worker(rank, world_size):
@@ -96,7 +145,7 @@ def main_worker(rank, world_size):
     ).to(rank) 
     
     img_size = (256,256)
-    batch_size = 50  #  This is per-GPU batch size**
+    batch_size = 64  #  This is per-GPU batch size**
 
     device = torch.device(f"cuda:{rank}")
 
@@ -133,9 +182,9 @@ def main_worker(rank, world_size):
         [[0, 0, 0], [0, -1, 0], [1, 0, 0]], [[0, 0, 0], [0, -1, 0], [0, 1, 0]], [[0, 0, 0], [0, -1, 0], [0, 0, 1]]
     ],dtype=torch.float32).to(device)
     
-    large_depth_map = AutoModelForDepthEstimation.from_pretrained("depth-anything/Depth-Anything-V2-Large-hf").to(device)
-    large_depth_map.eval()
-    large_depth_map_compiled = torch.compile(large_depth_map,mode="max-autotune")
+    # large_depth_map = AutoModelForDepthEstimation.from_pretrained("depth-anything/Depth-Anything-V2-Large-hf").to(device)
+    # large_depth_map.eval()
+    # large_depth_map_compiled = torch.compile(large_depth_map,mode="max-autotune")
     
     criterion = nn.SmoothL1Loss()
     mse_criterion = nn.MSELoss()
@@ -155,18 +204,18 @@ def main_worker(rank, world_size):
         running_loss = 0.0
         
         for i, data in enumerate(train_loader, 0):
-            images, labels = data
-            binary_labels = labels.to(device)
-            inputs = images.to(device)
+            binary_labels = data[1].to(device)
+            inputs = data[0].to(device)
+            depth_maps = data[-1].to(device)
 
             inputs_aug = gpu_augmentation_pipeline(inputs)
             optimizer.zero_grad()
             
             outputs = model(inputs_aug)
 
-            with torch.no_grad():
-                out = large_depth_map_compiled(inputs_aug)
-                depth_maps = out.predicted_depth.unsqueeze(1)
+            # with torch.no_grad():
+            #     out = large_depth_map_compiled(inputs_aug)
+            #     depth_maps = out.predicted_depth.unsqueeze(1)
             
             labels = get_labels(depth_maps, binary_labels)
             loss = customLoss(criterion, mse_criterion, outputs, labels, kernel_filter)
@@ -195,10 +244,11 @@ def main_worker(rank, world_size):
                 for i, data in enumerate(val_loader, 0):
                     binary_labels_test = data[1].to(device)
                     inputs_aug_test = gpu_augmentation_pipeline(data[0].to(device))
+                    depth_maps_test = data[-1].to(device)
                     outputs_test = model(inputs_aug_test)
 
-                    out = large_depth_map_compiled(inputs_aug_test)
-                    depth_maps_test = out.predicted_depth.unsqueeze(1)
+                    # out = large_depth_map_compiled(inputs_aug_test)
+                    # depth_maps_test = out.predicted_depth.unsqueeze(1)
 
                     labels_test = get_labels(depth_maps_test, binary_labels_test)
                     loss_test = customLoss(criterion, mse_criterion, outputs_test, labels_test, kernel_filter)
